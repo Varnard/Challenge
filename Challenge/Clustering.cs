a42d9d505f80282bd71e18dec05bf7ba41f4e2aa
Maciej Buzalski <varnard@vp.pl>
2017-04-26 23:46:58 +0200

    K-Means Clustering

---
+++ Challenge/Clustering.cs
@@ -0,0 +1,73 @@
+﻿using Accord.MachineLearning;
+using Accord.Math.Distances;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Challenge
+{
+    public class Clustering
+    {
+        List<List<int>> Clusters;
+
+        public Clustering(int[][] distanceMatrix, int k)
+        {
+            var dist = new VertexDistance(distanceMatrix);
+
+            Clusters = KMeansCluster(distanceMatrix.Length, dist, k);
+
+        }
+
+        protected List<List<int>> KMeansCluster(int n, IDistance distance, int k)
+        {
+            List<List<int>> results = new List<List<int>>();
+
+            double[][] vertices = new double[n][];
+
+            for (int i = 0; i < n; i++)
+            {
+                double[] vertex = new double[1];
+                vertex[0] = i + 1;
+                vertices[i] = vertex;
+            }
+
+            KMeans kmeans = new KMeans(k)
+            {
+                Distance = distance
+            };
+
+            var clusters = kmeans.Learn(vertices);
+
+            int[] decisions = clusters.Decide(vertices);
+
+            for (int i = 0; i < k; i++)
+            {
+                List<int> cluster = new List<int>();
+                for (int j = 0; j < n; j++)
+                {
+                    if (decisions[j] == i) cluster.Add(j);
+                }
+                results.Add(cluster);
+            }
+
+            return results;
+        }
+
+        public List<List<int>> getClusters()
+        {
+            return Clusters;
+        }
+
+        public void toConsole()
+        {
+            Clusters.ForEach(item =>
+            {
+                item.ForEach(Console.Write);
+                Console.WriteLine();
+            });
+        }
+
+    }
+}
---
+++ Challenge/VertexDistance.cs
@@ -0,0 +1,24 @@
+﻿using Accord.Math.Distances;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Challenge
+{
+    public class VertexDistance : IDistance
+    {
+        int[][] distanceMatrix { get; set; }
+
+        public VertexDistance(int[][] distanceMatrix)
+        {
+            this.distanceMatrix = distanceMatrix;
+        }
+
+        public double Distance(double[] x, double[] y)
+        {
+            return (double)distanceMatrix[(int)x[0]-1][(int)y[0]-1];
+        }
+    }
+}
