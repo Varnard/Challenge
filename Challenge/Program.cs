256aeb64d80df513e9249656cbc6fedd16a2b431
Maciej Buzalski <varnard@vp.pl>
2017-04-27 10:36:41 +0200

    Console Output

--- Challenge/Program.cs
+++ Challenge/Program.cs
@@ -10,10 +10,23 @@ namespace Challenge
     {
         static void Main(string[] args)
         {
+            List<int> a = new List<int>();
+            a.Add(1);
+            a.Add(2);
+            a.Add(3);
 
-        //asdf
+            List<int> b = new List<int>();
+            b.Add(4);
+            b.Add(5);
 
+            List<List<int>> clusters = new List<List<int>>();
 
+            clusters.Add(a);
+            clusters.Add(b);
+
+            Output.toConsole(clusters, 0.357);
+
+            Console.ReadKey();
         }
     }
 }
